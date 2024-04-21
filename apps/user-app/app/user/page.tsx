f25f2c768888d691f37ab5b17d1f195246d4cf84
saurabh gupta <saurabhgupta0122@gmail.com>
2024-04-22 01:43:22 +0530

    "update"

--- apps/user-app/app/user/page.tsx
+++ apps/user-app/app/user/page.tsx
@@ -25,6 +25,8 @@
           <button onClick={()=>signOut()}>Log Out</button>
         </div>
       </div>
+      <h1>{session.data ? `Hello ${session.data.user}` : 'You are not logged in'}</h1>
+
       {JSON.stringify(session)}
     </>
    ) 
